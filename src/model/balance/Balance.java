8364a3eabf742af4b60001d223033ed76ae8497f
Aga Furgalska <aga.furgalska@wp.pl>
2025-03-27 09:59:36 +0100

    model draft

---
+++ src/model/balance/Balance.java
@@ -0,0 +1,19 @@
+package model.balance;
+
+public class Balance
+{
+  private int amount;
+
+  public Balance(int amount){
+    this.amount = amount;
+  }
+
+  public void setAmount(int amount)
+  {
+    this.amount = amount;
+  }
+
+  public int getAmount(){
+    return amount;
+  }
+}
---
+++ src/model/user/Admin.java
@@ -0,0 +1,36 @@
+package model.user;
+
+public class Admin implements User
+{
+  private String username;
+  private String email;
+  private String password;
+
+  @Override public String getUsername()
+  {
+    return username;
+  }
+
+  @Override public void setUsername(String username)
+  {
+    this.username = username;
+  }
+
+  @Override public String getEmail()
+  {
+    return email;
+  }
+
+  @Override public void setEmail(String email)
+  {
+    this.email = email;
+  }
+
+  public String getPassword(){
+    return password;
+  }
+
+  public void setPassword(String password){
+    this.password = password;
+  }
+}
