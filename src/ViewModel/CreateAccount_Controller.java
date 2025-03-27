396d43e391c1e30c82b5bdcb3ef636d54f8ee056
BbyBruh <reiashtray@gmail.com>
2025-03-27 10:39:01 +0100

    GUI basic design: LoginAccount_Controller and CreateAccount_Controller

---
+++ src/ViewModel/CreateAccount_Controller.java
@@ -0,0 +1,25 @@
+package ViewModel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class CreateAccount_Controller
+{
+  @FXML
+  private TextField username;
+  @FXML
+  private TextField email;
+  @FXML
+  private TextField password;
+  @FXML
+  private TextField confirmPassword;
+  @FXML
+  private Button cancel;
+  @FXML
+  private Button proceed;
+
+  public CreateAccount_Controller()
+  {
+
+  }
+}
---
+++ src/ViewModel/LoginAccount_Controller.java
@@ -0,0 +1,24 @@
+package ViewModel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginAccount_Controller
+{
+  @FXML
+  private TextField username;
+  @FXML
+  private TextField email;
+  @FXML
+  private TextField password;
+  @FXML
+  private Button cancel;
+  @FXML
+  private Button proceed;
+
+  public LoginAccount_Controller()
+  {
+
+  }
+}
