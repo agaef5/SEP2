aa6bf68b2c9c6d6c9153c8d0d8d5d94427031901
mrgreenery <wilbergroen@gmail.com>
2025-05-03 17:35:49 +0200

    fixed some typos (length / lenght) created race create view started on vm, but need help on create racemanager made the admin panel. Decided we don't need a VM in that for now

--- src/client/ui/racerList/adminView/adminPanel/AdminPanelController.java
+++ src/client/ui/racerList/adminView/adminPanel/AdminPanelController.java
@@ -1,6 +1,37 @@
 package client.ui.racerList.adminView.adminPanel;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AdminPanelController
 {
+  @FXML private Button addRace;
+  @FXML private Button addHorse;
+  @FXML private Button editUser;
+
+  @FXML
+  private void initialize() {
+    addRace.setOnAction(e -> loadScene("/client/ui/racerList/adminView/race/CreateRace.fxml"));
+    addHorse.setOnAction(e -> loadScene("/client/ui/racerList/adminView/racer/CreateEditRacer.fxml"));
+//    editUser.setOnAction(e -> loadScene("/client/ui/racerList/adminView/user/EditUser.fxml"));
+  }
+
+  private void loadScene(String fxmlPath) {
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+      Parent root = loader.load();
+      Stage stage = (Stage) addRace.getScene().getWindow();
+      stage.setScene(new Scene(root));
+    } catch (IOException e) {
+      e.printStackTrace();
+
+    }
+  }
 
 }
--- src/client/ui/racerList/adminView/adminPanel/AdminPanelVM.java
+++
@@ -1,5 +0,0 @@
-package client.ui.racerList.adminView.adminPanel;
-
-public class AdminPanelVM
-{
-}
