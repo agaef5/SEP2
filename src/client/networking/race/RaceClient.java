a0ba589adbb7bad540bbd3ac81525441e88f4659
mrgreenery <wilbergroen@gmail.com>
2025-05-07 16:20:36 +0200

    updated the gui so queue is embedded. Whole lot of tiny adjustments, so this could be incorporated Made the label connect to the list Javadoc for gui

--- src/client/networking/race/RaceClient.java
+++ src/client/networking/race/RaceClient.java
@@ -10,18 +10,23 @@ import shared.GetRaceTracksRequest;
  */
 public interface RaceClient {
 
+   /**
+    * Sends a request to retrieve all races without any specific criteria.
+    * This method fetches the complete list of races from the server.
+    */
    void getRaceList();
 
    /**
     * Sends a request to retrieve a list of races based on the provided {@link GetRaceListRequest}.
+    * Allows filtering and customizing the race list retrieval.
     *
     * @param getRaceListRequest the request containing the criteria for retrieving races
     */
-
-
    void getRaceList(GetRaceListRequest getRaceListRequest);
+
    /**
     * Sends a request to create a new race using the provided {@link CreateRaceRequest}.
+    * After successful creation, the new race will be added to the race queue.
     *
     * @param createRaceRequest the request containing the details for the new race
     */
@@ -29,9 +34,9 @@ public interface RaceClient {
 
    /**
     * Sends a request to retrieve a list of available race tracks based on the provided {@link GetRaceTracksRequest}.
+    * These race tracks can be used when creating new races.
     *
     * @param getRaceTracksRequest the request containing the criteria for retrieving race tracks
     */
    void getRaceTracks(GetRaceTracksRequest getRaceTracksRequest);
-
 }
