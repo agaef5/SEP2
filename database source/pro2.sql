34b8cdf974a678a974b05b54999067200af728b0
mrgreenery <wilbergroen@gmail.com>
2025-05-08 14:32:17 +0200

    GUI for userLandingPage + changes some init methods to initialize

--- database source/pro2.sql
+++ database source/pro2.sql
@@ -1,54 +1,56 @@
-create schema sep2;
-set schema 'sep2';
+CREATE SCHEMA sep2;
+SET search_path TO sep2;
 
-create table sep2.user(
-    username varchar(10) primary key,
-    password_hash varchar,
-    email varchar,
-    role_id int
+
+CREATE TABLE sep2.user(
+    username VARCHAR(10) PRIMARY KEY,
+    password_hash VARCHAR,
+    email VARCHAR,
+    role_id INT
 );
 
-create table sep2.player(
-    username varchar(10) references sep2.user(username),
-    balance int
+CREATE TABLE sep2.player(
+    username VARCHAR(10) REFERENCES sep2.user(username),
+    balance INT
 );
 
-create table sep2.admin
-(
-    username varchar(10) references sep2.user(username),
-    id int primary key
-);
-
-create table sep2.horse(
-    id  serial primary key,
-    name varchar,
-    speedMin int,
-    speedMax int
+CREATE TABLE sep2.admin(
+    username VARCHAR(10) REFERENCES sep2.user(username),
+    id INT PRIMARY KEY
 );
 
 
-CREATE TABLE sep2.race (
-    id INT PRIMARY KEY,
-    admin_id INT REFERENCES sep2.admin(id),
+CREATE TABLE sep2.horse(
+    id SERIAL PRIMARY KEY,
     name VARCHAR,
-    status VARCHAR,
-    startTime DATE
+    speedMin INT,
+    speedMax INT
 );
 
-CREATE TABLE sep2.raceTrack (
-    race_id INT,
+CREATE TABLE sep2.raceTrack(
+    id SERIAL PRIMARY KEY,
     name VARCHAR,
     raceLength INT,
     location VARCHAR
 );
 
-INSERT INTO sep2.raceTrack (race_id, name, raceLength, location)
-VALUES (1, 'Midtown Madness', 1000, 'Aarhus'),
-       (2, 'Steelball Run', 10000, 'U.S.A.'),
-       (3, 'Dakar Rally', 2500, 'Sahara'),
-       (4, 'Dirt track', 3300, 'Sweden'),
-       (5, 'Ice track', 4000, 'Syberia'),
-       (6, 'Hot', 666, 'Hell')
 
-DROP TABLE IF EXISTS sep2.raceTrack;
-DROP TABLE IF EXISTS sep2.race;
+CREATE TABLE sep2.race(
+    id INT PRIMARY KEY,
+    admin_id INT REFERENCES sep2.admin(id),
+    racetrack_id INT REFERENCES sep2.raceTrack(id),
+    name VARCHAR,
+    status VARCHAR,
+    startTime DATE
+);
+
+
+INSERT INTO sep2.raceTrack (name, raceLength, location)
+VALUES
+    ('Midtown Madness', 1000, 'Aarhus'),
+    ('Steelball Run', 10000, 'U.S.A.'),
+    ('Dakar Rally', 2500, 'Sahara'),
+    ('Dirt track', 3300, 'Sweden'),
+    ('Ice track', 4000, 'Syberia'),
+    ('Hot', 666, 'Hell');
+
